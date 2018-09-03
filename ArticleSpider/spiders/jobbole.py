2f31159528543ce89fa1b1d4b3a0511437919783
chenjiayu <chenlal@126.com>
2018-09-03 20:20:21 +0800

    改用css-selector获取元素

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -10,25 +10,57 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
-        title = response.xpath("//div[@class ='entry-header']/h1/text()").extract()[0]
-        creat_date = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace(
-            "·", "").strip()
-        prise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
-        collection_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0].strip()
+
+        # xpath方式
+
+        # title = response.xpath("//div[@class ='entry-header']/h1/text()").extract()[0]
+        # creat_date = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace(
+        #     "·", "").strip()
+        # prise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
+        # collection_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0].strip()
+        # re_match_collection = re.match("(\d+).*", collection_nums)
+        # if re_match_collection:
+        #     collection_nums = re_match_collection.group(1)
+        # else:
+        #     collection_nums = '0'
+        # comment_nums = response.xpath("//a[@href='#article-comment']/text()").extract()[0].strip()
+        # re_match_collection = re.match("(\d+).*", comment_nums)
+        # if re_match_collection:
+        #     comment_nums = re_match_collection.group(1)
+        # else:
+        #     comment_nums = '0'
+        # content = response.xpath("//div[@class = 'entry']").extract()
+        # tag_list = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/a/text()").extract()
+        # tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        # tags = '·'.join(tag_list)
+        # creator = response.xpath("//div[@class = 'copyright-area']/a/text()").extract()
+
+
+
+
+
+
+
+
+        # css方式
+
+        title = response.css(".entry-header h1::text").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·", "").strip()
+        prise_nums = response.css("span.vote-post-up h10::text").extract()[0]
+        collection_nums = response.css("span.bookmark-btn::text").extract()[0].strip()
         re_match_collection = re.match("(\d+).*", collection_nums)
         if re_match_collection:
             collection_nums = re_match_collection.group(1)
         else:
             collection_nums = '0'
-        comment_nums = response.xpath("//a[@href='#article-comment']/text()").extract()[0].strip()
+        comment_nums = response.css("a[href='#article-comment']::text").extract()[0].strip()
         re_match_collection = re.match("(\d+).*", comment_nums)
         if re_match_collection:
             comment_nums = re_match_collection.group(1)
         else:
             comment_nums = '0'
-        content = response.xpath("//div[@class = 'entry']").extract()
-        tag_list = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/a/text()").extract()
+        creator = response.css(".copyright-area a::text").extract()[0]
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         tags = '·'.join(tag_list)
-        creator = response.xpath("//div[@class = 'copyright-area']/a/text()").extract()
         pass
