d9adbf05d46666ab8073d4e7b81276e3891a764f
chenjiayu <chenlal@126.com>
2018-09-03 13:49:40 +0800

    获取出处和文章分类

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -7,11 +7,12 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/113659/']
+    start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
         title = response.xpath("//div[@class ='entry-header']/h1/text()").extract()[0]
-        creat_date = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·", "").strip()
+        creat_date = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace(
+            "·", "").strip()
         prise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
         collection_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0].strip()
         re_match_collection = re.match("(\d+).*", collection_nums)
@@ -26,4 +27,9 @@ class JobboleSpider(scrapy.Spider):
             comment_nums = re_match_collection.group(1)
         else:
             comment_nums = '0'
+        content = response.xpath("//div[@class = 'entry']").extract()
+        tag_list = response.xpath("//p[@class = 'entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = '·'.join(tag_list)
+        creator = response.xpath("//div[@class = 'copyright-area']/a/text()").extract()
         pass
